eb3507f718b3a608553c22c0452b5aa18bebae42
Bartosz Taczala <bartosz.taczala@mobica.com>
2017-05-30 00:01:34 +0200

    Prepare for ut

--- src/locxxate.cpp
+++ src/locxxate.cpp
@@ -3,18 +3,33 @@
 #include <algorithm>
 #include <iostream>
 
+namespace fs = std::experimental::filesystem;
 std::vector<std::experimental::filesystem::path> fetchAllFiles(
     const std::string& dir, const std::vector<std::string>& excluded) {
-    namespace fs = std::experimental::filesystem;
     const std::string actualDir = dir.empty() ? "" : dir;
 
+    return filter(loadDirs(actualDir), excluded);
+}
+
+std::vector<std::experimental::filesystem::path> loadDirs(
+    const std::string& dir) {
+    std::vector<std::experimental::filesystem::path> dirs;
+    std::copy(fs::recursive_directory_iterator(dir),
+              fs::recursive_directory_iterator(), std::back_inserter(dirs));
+
+    return dirs;
+}
+std::vector<std::experimental::filesystem::path> filter(
+    const std::vector<std::experimental::filesystem::path>& entries,
+    const std::vector<std::string>& excluded) {
     std::vector<std::experimental::filesystem::path> dirs;
     fs::path p;
+
     regex_t exp;
     regmatch_t match;
     const auto filter = [&excluded, &exp, &match](auto fs) {
         for (auto e : excluded) {
-            auto s = fs.path().string();
+            auto s = fs.string();
             regcomp(&exp, e.c_str(), REG_EXTENDED);
             if (regexec(&exp, s.c_str(), 1, &match, 0) == 0) {
                 return false;
@@ -22,11 +37,8 @@ std::vector<std::experimental::filesystem::path> fetchAllFiles(
         }
         return true;
     };
-
-    std::copy_if(fs::recursive_directory_iterator(dir),
-                 fs::recursive_directory_iterator(), std::back_inserter(dirs),
+    std::copy_if(entries.begin(), entries.end(), std::back_inserter(dirs),
                  filter);
 
-    std::cout << "entries " << dirs.size() << std::endl;
     return dirs;
 }
--- src/locxxate.h
+++ src/locxxate.h
@@ -9,4 +9,11 @@ std::vector<std::experimental::filesystem::path> fetchAllFiles(
     const std::string& defaultDir,
     const std::vector<std::string>& excluded = {});
 
+std::vector<std::experimental::filesystem::path> loadDirs(
+    const std::string& defaultDir);
+
+std::vector<std::experimental::filesystem::path> filter(
+    const std::vector<std::experimental::filesystem::path>& entries , const
+        std::vector<std::string>& excluded = {});
+
 #endif /* end of include guard: LOCXXATE_H_B86LVQXX */
